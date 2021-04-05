22a5f197761e570ef6d40ce9d31273bd9dc48c59
cjvilla <CelineVillareal2020@northwestern.edu>
2021-04-05 12:04:33 -0500

    added css files and images

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,5 @@
 var sightings = data;
-var button = d3.select("#datetime");
+var button = d3.select("#button");
 var form = d3.select("#form");
 
 button.on("click", runEnter);
@@ -7,4 +7,18 @@ form.on("submit", runEnter);
 
 function runEnter() {
     d3.event.preventDefault();
+    var inputElement = d3.select(".form-control");
+
+    var inputValue = inputElement.property('value').toLowerCase();
+    console.log(inputValue);
+
+    var tableElement = d3.select("table");
+    tableElement.attr('class', 'table table-striped');
+    var tobodyElement = d3.select('tbody');
+
+    var dataArray = (s => {
+        var trElement = tbodyElement.append("tr");
+        trElement.append("td").text(s);
+    })
+
 }
